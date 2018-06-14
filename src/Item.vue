ff4f6a0c44ce58835747a2981edd2be398fe3ff0
Filip Kostovski <filip@trackconcepts.com>
2018-06-15 00:43:07 +0200

    First commit

---
+++ src/Item.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col-md-3">
+    <div class="card">
+      <img :src="image" :alt="name" class="card-img-top">
+      <div class="card-body">
+        <h4 class="card-title">{{ name }}</h4>
+        <div class="card-text">{{ price | dollars }}</div>
+        <div class="row justify-content-end">
+          <button class="btn btn-primary" @click="addToCart(invId)">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { dollars } from './filters';
+
+export default {
+  name: 'item',
+  props: ['id', 'name', 'image', 'price', 'invId'],
+  filters: {
+    dollars,
+  },
+  methods: {
+      addToCart(invId) {
+          this.$store.dispatch('addToCart', invId);
+      }
+  }
+};
+</script>
